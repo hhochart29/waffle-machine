024fab000dcdddf0f3885227adb606fb7c2e3084
Hervé <contact@herve-hochart.fr>
2020-06-17 19:39:26 +0200

    :wrench: (Tailwind purge)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
-    content: ['components/**/*.vue']
+    content: ['**/*.vue']
   },
   theme: {
     screens: {
